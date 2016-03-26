5b85347ab6058e579daccbc2895fda4a8f799f7b
Patrick Roche <patrick.l.roche@gmail.com>
2016-03-26 18:23:29 -0400

    feat: Show dictionary json keys and values using console.log
    Original example of showing JSON was removed and replaced
    with example that prints a dictionary subset to the
    console.

---
+++ dictionary-small.js
@@ -0,0 +1,6 @@
+var dictionary = {
+    "DIPLOBLASTIC": "Characterizing the ovum when it has two primary germinallayers.",
+    "DEFIGURE": "To delineate. [Obs.]These two stones as they are here defigured. Weever.",
+    "LOMBARD": "Of or pertaining to Lombardy, or the inhabitants of Lombardy.",
+    "BAHAISM": "The religious tenets or practices of the Bahais."
+};
