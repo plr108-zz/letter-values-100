ba026616a656cc4de25641d886a1f057a0c60e62
Patrick Roche <patrick.l.roche@gmail.com>
2016-03-27 20:56:05 -0400

    feat: display wordList in #results div

--- app.js
+++ app.js
@@ -9,29 +9,28 @@ function makeWordList(arr) {
 
     bubbleSort(wordList);
 
-    return wordList;
+    document.getElementById("results").innerHTML = wordList;
 }
 
 // swap, bubble sort source: https://github.com/benoitvallon/computer-science-in-javascript/blob/master/sorting-algorithms-in-javascript/bubble-sort.js
 function swap(array, i, j) {
-  var temp = array[i];
-  array[i] = array[j];
-  array[j] = temp;
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
 }
 
 function bubbleSort(array) {
-  var swapped;
-  do {
-    swapped = false;
-    for(var i = 0; i < array.length; i++) {
-      if(array[i] && array[i + 1] && array[i] > array[i + 1]) {
-        swap(array, i, i + 1);
-        swapped = true;
-      }
-    }
-  } while(swapped);
-  return array;
+    var swapped;
+    do {
+        swapped = false;
+        for (var i = 0; i < array.length; i++) {
+            if (array[i] && array[i + 1] && array[i] > array[i + 1]) {
+                swap(array, i, i + 1);
+                swapped = true;
+            }
+        }
+    } while (swapped);
+    return array;
 }
 
-var sortedWordList = makeWordList(dictionary);
-console.log(sortedWordList);
+makeWordList(dictionary);
